fa23a624bf9eb5719a6f229302ff10182e3c9977
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-01-13 12:53:38 +0100

    aggiunto tailwind

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <ul>
     <li>
-      <a href="{{ route('comics.index') }}">Home</a>
+      <a href="{{ route('comics.index') }}" class="text-3xl text-slate-600">Home</a>
     </li>
     <li>
       <a href="{{ route('comics.create') }}">Crea un nuovo Comic</a>
